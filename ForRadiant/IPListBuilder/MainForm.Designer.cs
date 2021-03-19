f52e4c46793bf6f4197bb083072826b1b8955902
Chau Do Hoang <hoangchaudo89@gmail.com>
2021-03-19 15:36:59 +0700

    add IP List builder

---
+++ ForRadiant/IPListBuilder/MainForm.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace IPListBuilder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabIPScanner = new System.Windows.Forms.TabPage();
+            this.btnGenerate1 = new System.Windows.Forms.Button();
+            this.cbIPList1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabCopyWhiz = new System.Windows.Forms.TabPage();
+            this.cbExtendedPath = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnGenerate2 = new System.Windows.Forms.Button();
+            this.cbIPList2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabRealVNC = new System.Windows.Forms.TabPage();
+            this.btnGenerate3 = new System.Windows.Forms.Button();
+            this.cbIPList3 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabIPScanner.SuspendLayout();
+            this.tabCopyWhiz.SuspendLayout();
+            this.tabRealVNC.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabIPScanner);
+            this.tabControl1.Controls.Add(this.tabCopyWhiz);
+            this.tabControl1.Controls.Add(this.tabRealVNC);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(445, 126);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabIPScanner
+            // 
+            this.tabIPScanner.Controls.Add(this.btnGenerate1);
+            this.tabIPScanner.Controls.Add(this.cbIPList1);
+            this.tabIPScanner.Controls.Add(this.label1);
+            this.tabIPScanner.Location = new System.Drawing.Point(4, 22);
+            this.tabIPScanner.Name = "tabIPScanner";
+            this.tabIPScanner.Padding = new System.Windows.Forms.Padding(3);
+            this.tabIPScanner.Size = new System.Drawing.Size(437, 100);
+            this.tabIPScanner.TabIndex = 0;
+            this.tabIPScanner.Text = "Advance IP Scanner";
+            this.tabIPScanner.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerate1
+            // 
+            this.btnGenerate1.Location = new System.Drawing.Point(9, 33);
+            this.btnGenerate1.Name = "btnGenerate1";
+            this.btnGenerate1.Size = new System.Drawing.Size(417, 31);
+            this.btnGenerate1.TabIndex = 2;
+            this.btnGenerate1.Text = "Generate";
+            this.btnGenerate1.UseVisualStyleBackColor = true;
+            this.btnGenerate1.Click += new System.EventHandler(this.btnGenerate1_Click);
+            // 
+            // cbIPList1
+            // 
+            this.cbIPList1.FormattingEnabled = true;
+            this.cbIPList1.Location = new System.Drawing.Point(48, 6);
+            this.cbIPList1.Name = "cbIPList1";
+            this.cbIPList1.Size = new System.Drawing.Size(378, 21);
+            this.cbIPList1.TabIndex = 1;
+            this.cbIPList1.DropDown += new System.EventHandler(this.cbIPList1_DropDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP List";
+            // 
+            // tabCopyWhiz
+            // 
+            this.tabCopyWhiz.Controls.Add(this.cbExtendedPath);
+            this.tabCopyWhiz.Controls.Add(this.label4);
+            this.tabCopyWhiz.Controls.Add(this.btnGenerate2);
+            this.tabCopyWhiz.Controls.Add(this.cbIPList2);
+            this.tabCopyWhiz.Controls.Add(this.label2);
+            this.tabCopyWhiz.Location = new System.Drawing.Point(4, 22);
+            this.tabCopyWhiz.Name = "tabCopyWhiz";
+            this.tabCopyWhiz.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCopyWhiz.Size = new System.Drawing.Size(437, 100);
+            this.tabCopyWhiz.TabIndex = 1;
+            this.tabCopyWhiz.Text = "CopyWhiz";
+            this.tabCopyWhiz.UseVisualStyleBackColor = true;
+            // 
+            // cbExtendedPath
+            // 
+            this.cbExtendedPath.FormattingEnabled = true;
+            this.cbExtendedPath.Location = new System.Drawing.Point(89, 33);
+            this.cbExtendedPath.Name = "cbExtendedPath";
+            this.cbExtendedPath.Size = new System.Drawing.Size(337, 21);
+            this.cbExtendedPath.TabIndex = 6;
+            this.cbExtendedPath.Text = "\\Program\\RVS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Extended Path";
+            // 
+            // btnGenerate2
+            // 
+            this.btnGenerate2.Location = new System.Drawing.Point(9, 60);
+            this.btnGenerate2.Name = "btnGenerate2";
+            this.btnGenerate2.Size = new System.Drawing.Size(417, 31);
+            this.btnGenerate2.TabIndex = 4;
+            this.btnGenerate2.Text = "Generate";
+            this.btnGenerate2.UseVisualStyleBackColor = true;
+            this.btnGenerate2.Click += new System.EventHandler(this.btnGenerate2_Click);
+            // 
+            // cbIPList2
+            // 
+            this.cbIPList2.FormattingEnabled = true;
+            this.cbIPList2.Location = new System.Drawing.Point(48, 6);
+            this.cbIPList2.Name = "cbIPList2";
+            this.cbIPList2.Size = new System.Drawing.Size(378, 21);
+            this.cbIPList2.TabIndex = 3;
+            this.cbIPList2.DropDown += new System.EventHandler(this.cbIPList2_DropDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "IP List";
+            // 
+            // tabRealVNC
+            // 
+            this.tabRealVNC.Controls.Add(this.btnGenerate3);
+            this.tabRealVNC.Controls.Add(this.cbIPList3);
+            this.tabRealVNC.Controls.Add(this.label3);
+            this.tabRealVNC.Location = new System.Drawing.Point(4, 22);
+            this.tabRealVNC.Name = "tabRealVNC";
+            this.tabRealVNC.Size = new System.Drawing.Size(437, 100);
+            this.tabRealVNC.TabIndex = 2;
+            this.tabRealVNC.Text = "Real VNC";
+            this.tabRealVNC.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerate3
+            // 
+            this.btnGenerate3.Location = new System.Drawing.Point(9, 33);
+            this.btnGenerate3.Name = "btnGenerate3";
+            this.btnGenerate3.Size = new System.Drawing.Size(417, 31);
+            this.btnGenerate3.TabIndex = 6;
+            this.btnGenerate3.Text = "Generate";
+            this.btnGenerate3.UseVisualStyleBackColor = true;
+            this.btnGenerate3.Click += new System.EventHandler(this.btnGenerate3_Click);
+            // 
+            // cbIPList3
+            // 
+            this.cbIPList3.FormattingEnabled = true;
+            this.cbIPList3.Location = new System.Drawing.Point(48, 6);
+            this.cbIPList3.Name = "cbIPList3";
+            this.cbIPList3.Size = new System.Drawing.Size(378, 21);
+            this.cbIPList3.TabIndex = 5;
+            this.cbIPList3.DropDown += new System.EventHandler(this.cbIPList3_DropDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "IP List";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 146);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "IP List Builder";
+            this.tabControl1.ResumeLayout(false);
+            this.tabIPScanner.ResumeLayout(false);
+            this.tabIPScanner.PerformLayout();
+            this.tabCopyWhiz.ResumeLayout(false);
+            this.tabCopyWhiz.PerformLayout();
+            this.tabRealVNC.ResumeLayout(false);
+            this.tabRealVNC.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabIPScanner;
+        private System.Windows.Forms.TabPage tabCopyWhiz;
+        private System.Windows.Forms.TabPage tabRealVNC;
+        private System.Windows.Forms.ComboBox cbIPList1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbIPList2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbIPList3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGenerate1;
+        private System.Windows.Forms.Button btnGenerate2;
+        private System.Windows.Forms.Button btnGenerate3;
+        private System.Windows.Forms.ComboBox cbExtendedPath;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ForRadiant/IPListBuilder/MainForm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IPListBuilder
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void cbIPList1_DropDown(object sender, EventArgs e)
+        {
+            cbIPList1.Items.Clear();
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.csv");
+            foreach (string file in files)
+            {
+                cbIPList1.Items.Add(file);
+            }
+        }
+
+        private void cbIPList2_DropDown(object sender, EventArgs e)
+        {
+            cbIPList2.Items.Clear();
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.csv");
+            foreach (string file in files)
+            {
+                cbIPList2.Items.Add(file);
+            }
+        }
+
+        private void cbIPList3_DropDown(object sender, EventArgs e)
+        {
+            cbIPList3.Items.Clear();
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.csv");
+            foreach (string file in files)
+            {
+                cbIPList3.Items.Add(file);
+            }
+        }
+
+        private void btnGenerate1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(cbIPList1.Text))
+            {
+                string[] lines = File.ReadAllLines(cbIPList1.Text);
+                string filename = Path.GetFileNameWithoutExtension(cbIPList1.Text);
+                List<string> newfile = new List<string>();
+                newfile.Add("<?xml version=\"1.0\" encoding=\"UTF - 8\"?>");
+                newfile.Add("<Advanced_IP_scanner>");
+                
+                foreach (string line in lines)
+                {
+                    string newline = "   <row status=\"alive\" ip=\"" + line.Split(',')[1] + "\" has_http=\"0\" is_http8080=\"0\" has_https=\"0\" has_ftp=\"0\" has_rdp=\"0\" sIsDNSScan=\"0\" alias=\"" + line.Split(',')[0] + "\" expanded=\"0\"></row>";
+                    newfile.Add(newline);
+                }
+
+                newfile.Add("</Advanced_IP_scanner>");
+                File.WriteAllLines(filename+".xml", newfile);
+                MessageBox.Show("Generated: " + Path.GetFullPath(filename + ".xml"));
+            }
+
+        }
+
+        private void btnGenerate2_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(cbIPList2.Text))
+            {
+                string[] lines = File.ReadAllLines(cbIPList2.Text);
+                string filename = Path.GetFileNameWithoutExtension(cbIPList2.Text);
+                List<string> newfile = new List<string>();
+                newfile.Add("<?xml version=\"1.0\"?>");
+                newfile.Add("<Copywhiz Action=\"Copy\" ShowLog=\"Never\" LogContent=\"1\" RunSilently=\"False\">");
+                newfile.Add("   <Filter IncludeFilesMask=\"\" ExcludeFilesMask=\"\" IncludeFoldersMask=\"\" ExcludeFoldersMask=\"\" DateFilter=\"0\" FilterBySize=\"False\" CopyAllFilesToSingleFolder=\"False\" CreateFolderStructureOnly=\"False\" SkipEmptyFolders=\"False\" CreateFullFolderHierarchy=\"False\"/>");
+                newfile.Add("   <Source SourceFolder=\"\"></Source>");
+                newfile.Add("   <FileTransfer FileAlreadyExistsAction=\"1\" FileAlreadyExistsSkipIdentical=\"True\" FileAlreadyExistsSkipNewer=\"True\" FileAlreadyExistsRenameSuffix=\"\">");
+                newfile.Add("       <Destinations>");
+                foreach (string line in lines)
+                {
+                    string newline = "          <FileFolder Path=\"\\\\" + line.Split(',')[1] + cbExtendedPath.Text + "\"/>";
+                    newfile.Add(newline);
+                }
+                newfile.Add("       </Destinations>");
+                newfile.Add("   </FileTransfer>");
+                newfile.Add("</Copywhiz>");
+                File.WriteAllLines(filename + ".czml", newfile);
+                MessageBox.Show("Generated: " + Path.GetFullPath(filename + ".czml"));
+            }
+        }
+
+        private void btnGenerate3_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(cbIPList3.Text))
+            {
+                string[] lines = File.ReadAllLines(cbIPList3.Text);
+                foreach (string line in lines)
+                {
+                    string path = @"VNC addresses\" + line.Split(',')[0] + ".vnc";
+                    if (!Directory.Exists(@"VNC addresses"))
+                    {
+                        Directory.CreateDirectory(@"VNC addresses");
+                    }
+                    string contents = "ConnMethod=tcp" + Environment.NewLine+ "FriendlyName=" + line.Split(',')[0] + Environment.NewLine + "Host=" + line.Split(',')[1] + Environment.NewLine + "RelativePtr=0";
+                    File.WriteAllText(path, contents);
+                }
+                MessageBox.Show("Generated: " + Path.GetFullPath(@"VNC addresses"));
+
+            }
+        }
+    }
+}
