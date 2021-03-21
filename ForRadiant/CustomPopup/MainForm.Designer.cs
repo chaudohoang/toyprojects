2d2e9849c683c85dd6bf0dbcd51ee6e5a088d9cd
Chau Do Hoang <hoangchaudo89@gmail.com>
2021-03-21 15:50:19 +0700

    etc

---
+++ ForRadiant/CustomPopup/MainForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace CustomPopup
+{
+    partial class ErrorBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+
+        {
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.WindowTextLabel = new System.Windows.Forms.Label();
+            this._MinButton = new CustomPopup.ButtonZ();
+            this._CloseButton = new CustomPopup.ButtonZ();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.TopPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.MediumBlue;
+            this.TopPanel.Controls.Add(this.WindowTextLabel);
+            this.TopPanel.Controls.Add(this._MinButton);
+            this.TopPanel.Controls.Add(this._CloseButton);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(355, 32);
+            this.TopPanel.TabIndex = 0;
+            this.TopPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseDown);
+            this.TopPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseMove);
+            this.TopPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseUp);
+            // 
+            // WindowTextLabel
+            // 
+            this.WindowTextLabel.AutoSize = true;
+            this.WindowTextLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WindowTextLabel.ForeColor = System.Drawing.Color.White;
+            this.WindowTextLabel.Location = new System.Drawing.Point(7, 4);
+            this.WindowTextLabel.Name = "WindowTextLabel";
+            this.WindowTextLabel.Size = new System.Drawing.Size(64, 25);
+            this.WindowTextLabel.TabIndex = 4;
+            this.WindowTextLabel.Text = "Error !";
+            this.WindowTextLabel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.WindowTextLabel_MouseDown);
+            this.WindowTextLabel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.WindowTextLabel_MouseMove);
+            this.WindowTextLabel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.WindowTextLabel_MouseUp);
+            // 
+            // _MinButton
+            // 
+            this._MinButton.BackColor = System.Drawing.Color.MediumBlue;
+            this._MinButton.BZBackColor = System.Drawing.Color.MediumBlue;
+            this._MinButton.DisplayText = "_";
+            this._MinButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this._MinButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._MinButton.ForeColor = System.Drawing.Color.White;
+            this._MinButton.Location = new System.Drawing.Point(289, 3);
+            this._MinButton.MouseClickColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(180)))), ((int)(((byte)(200)))));
+            this._MinButton.MouseHoverColor = System.Drawing.Color.DarkBlue;
+            this._MinButton.Name = "_MinButton";
+            this._MinButton.Size = new System.Drawing.Size(31, 24);
+            this._MinButton.TabIndex = 3;
+            this._MinButton.Text = "_";
+            this._MinButton.TextLocation_X = 6;
+            this._MinButton.TextLocation_Y = -20;
+            this._MinButton.UseVisualStyleBackColor = false;
+            this._MinButton.Click += new System.EventHandler(this._MinButton_Click);
+            // 
+            // _CloseButton
+            // 
+            this._CloseButton.BackColor = System.Drawing.Color.MediumBlue;
+            this._CloseButton.BZBackColor = System.Drawing.Color.MediumBlue;
+            this._CloseButton.DisplayText = "X";
+            this._CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this._CloseButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._CloseButton.ForeColor = System.Drawing.Color.White;
+            this._CloseButton.Location = new System.Drawing.Point(320, 3);
+            this._CloseButton.MouseClickColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(180)))), ((int)(((byte)(200)))));
+            this._CloseButton.MouseHoverColor = System.Drawing.Color.DarkBlue;
+            this._CloseButton.Name = "_CloseButton";
+            this._CloseButton.Size = new System.Drawing.Size(31, 24);
+            this._CloseButton.TabIndex = 2;
+            this._CloseButton.Text = "X";
+            this._CloseButton.TextLocation_X = 6;
+            this._CloseButton.TextLocation_Y = -1;
+            this._CloseButton.UseVisualStyleBackColor = false;
+            this._CloseButton.Click += new System.EventHandler(this._CloseButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MediumBlue;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(343, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(12, 395);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MediumBlue;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 32);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(12, 395);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.MediumBlue;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(12, 415);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(331, 12);
+            this.panel3.TabIndex = 3;
+            // 
+            // ErrorBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(355, 427);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.TopPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ErrorBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Error";
+            this.TopPanel.ResumeLayout(false);
+            this.TopPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Panel TopPanel;
+        private ButtonZ _MinButton;
+        private ButtonZ _CloseButton;
+        private System.Windows.Forms.Label WindowTextLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
---
+++ ForRadiant/CustomPopup/Testform.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomPopup
+{
+    public partial class Testform : Form
+    {
+        public Testform()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ErrorBox.Show("hhhhhhhhhhhhhhhhhhhhhhhh");
+        }
+    }
+}
