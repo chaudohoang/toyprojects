a197241d68b310379e7d463ba5d4070988ed711b
Chau Do Hoang <hoangchaudo89@gmail.com>
2021-04-17 17:09:19 +0700

    etc

---
+++ SlideIt/SlideIt/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace SlideIt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Difficulty";
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(65, 6);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(20, 20);
+            this.txtX.TabIndex = 1;
+            this.txtX.Text = "4";
+            this.txtX.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "x";
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(109, 6);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(20, 20);
+            this.txtY.TabIndex = 3;
+            this.txtY.Text = "4";
+            this.txtY.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(15, 32);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(114, 35);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "Load Picture";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(145, 77);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SlideIt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ SlideIt/SlideIt/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlideIt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            ArrayList alAllButtons;
+            ArrayList ilSmallImages;
+            Bitmap MainBitmap;
+            Point EmptyPoint = new Point();
+           
+            int resolution = Int32.Parse(txtX.Text) * Int32.Parse(txtY.Text);
+            Form pic = new Form();
+            pic.AutoSize = true;
+            pic.Dock = DockStyle.Fill;
+            Panel Image = new Panel();
+            Image.Location = new Point(10, 10);
+            Image.Size = new Size(640, 640);
+            Panel gamemenu = new Panel();
+            gamemenu.Dock = DockStyle.Fill;
+            gamemenu.Location = new Point(10, 10+Image.Height+10);
+            gamemenu.Size = new Size(640, 10);
+            Button btnrestart = new Button();
+            btnrestart.Text = "Restart";
+            Button btnhint = new Button();
+            btnhint.Text = "Hint";
+            gamemenu.Controls.Add(btnrestart);
+            gamemenu.Controls.Add(btnhint);
+            Point curPos = new Point(10, 10);
+            int totButtons = gamemenu.Controls.OfType<Button>().Count();
+            foreach (Button but in gamemenu.Controls.OfType<Button>())
+            {
+                but.Width = gamemenu.Width / totButtons;
+                but.Location = curPos;
+                curPos = new Point(curPos.X + but.Width, 10);
+            }
+            pic.Controls.Add(Image);
+            pic.Controls.Add(gamemenu);
+            pic.Show();
+            using (OpenFileDialog img = new OpenFileDialog())
+            {
+                if (img.ShowDialog() == DialogResult.OK)
+                {
+                    MainBitmap = new Bitmap(320, 320);
+                    MainBitmap = (Bitmap)Bitmap.FromFile(img.FileName);
+                    Image.BackgroundImage = (Image)(MainBitmap);
+                }
+
+            }
+
+        }
+
+
+    }
+}
