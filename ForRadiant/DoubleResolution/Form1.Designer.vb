9ebaf394b8714ba90c15c82c9013f62953ec5902
Chau Do Hoang <hoangchaudo89@gmail.com>
2021-07-01 15:05:55 +0700

    new program

---
+++ ForRadiant/DoubleResolution/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RunButton = New System.Windows.Forms.Button()
+        Me.inputTextBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.outputTextBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'RunButton
+        '
+        Me.RunButton.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.RunButton.Location = New System.Drawing.Point(12, 60)
+        Me.RunButton.Name = "RunButton"
+        Me.RunButton.Size = New System.Drawing.Size(428, 29)
+        Me.RunButton.TabIndex = 4
+        Me.RunButton.Text = "Run"
+        Me.RunButton.UseVisualStyleBackColor = True
+        '
+        'inputTextBox
+        '
+        Me.inputTextBox.AllowDrop = True
+        Me.inputTextBox.Location = New System.Drawing.Point(89, 8)
+        Me.inputTextBox.Name = "inputTextBox"
+        Me.inputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.inputTextBox.Size = New System.Drawing.Size(351, 20)
+        Me.inputTextBox.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 11)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Input Files"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 37)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(71, 13)
+        Me.Label5.TabIndex = 18
+        Me.Label5.Text = "Output Folder"
+        '
+        'outputTextBox
+        '
+        Me.outputTextBox.Location = New System.Drawing.Point(89, 34)
+        Me.outputTextBox.Name = "outputTextBox"
+        Me.outputTextBox.Size = New System.Drawing.Size(351, 20)
+        Me.outputTextBox.TabIndex = 17
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(453, 97)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.outputTextBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.inputTextBox)
+        Me.Controls.Add(Me.RunButton)
+        Me.Name = "Form1"
+        Me.Text = "DoubleResolution"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RunButton As Button
+    Friend WithEvents inputTextBox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents outputTextBox As TextBox
+End Class
---
+++ ForRadiant/DoubleResolution/Form1.vb
@@ -0,0 +1,126 @@
+﻿Imports System.Text
+Imports System.Reflection
+Imports System.IO
+
+Public Class Form1
+
+    Private Sub SetVersionInfo()
+
+        Dim ass As System.Reflection.Assembly = System.Reflection.Assembly.GetExecutingAssembly()
+        Dim ver As System.Version = ass.GetName().Version
+        Dim startDate As DateTime = New Date(2000, 1, 1)
+        Dim diffDays As Integer = ver.Build
+        Dim computedDate As DateTime = startDate.AddDays(diffDays)
+        Dim lastBuilt As String = computedDate.ToShortDateString()
+        'Me.Text = (Me.Text & " " & ver.Major & "." & ver.Minor & "." & ver.Build & "." & ver.Revision & " (" & lastBuilt & ")")
+        Me.Text = (Me.Text & " " & ver.Major & "." & ver.Minor & "." & ver.Build & "." & ver.Revision)
+
+    End Sub
+    Private Sub RunButton_Click(sender As Object, e As EventArgs) Handles RunButton.Click
+        RunButton.Enabled = False
+
+        If inputTextBox.Text = "" Then
+            MessageBox.Show("input files are empty")
+            RunButton.Enabled = True
+            Exit Sub
+        End If
+        If outputTextBox.Text = "" Then
+            MessageBox.Show("output folder is empty")
+            RunButton.Enabled = True
+            Exit Sub
+        End If
+        If Not IO.Directory.Exists(outputTextBox.Text) Then
+            Try
+                IO.Directory.CreateDirectory(outputTextBox.Text)
+            Catch ex As Exception
+                MessageBox.Show("error making output folder")
+                RunButton.Enabled = True
+                Exit Sub
+            End Try
+        End If
+
+        Dim inputfile As String = inputTextBox.Text
+
+        Dim newrow As Integer
+        Dim newcolumn As Integer
+        Dim outputfile As String
+        Dim header(7) As String
+
+        newrow = File.ReadAllLines(inputfile).Length - 8
+        newcolumn = (File.ReadAllLines(inputfile)(8).Split(New String() {ControlChars.Tab}, StringSplitOptions.None).Count - 1) * 2
+        Dim newImage(newcolumn - 1, newrow - 1) As Single
+        Try
+            Using fs As New IO.StreamReader(inputfile)
+
+                header(0) = fs.ReadLine()
+                header(1) = fs.ReadLine()
+                header(2) = fs.ReadLine()
+                header(3) = fs.ReadLine()
+                header(4) = fs.ReadLine()
+                header(5) = fs.ReadLine()
+                header(6) = fs.ReadLine()
+                header(7) = fs.ReadLine()
+
+                Dim row As Integer = 0
+                Do While Not fs.EndOfStream
+
+                    Dim Data() As String = fs.ReadLine.Split(New String() {ControlChars.Tab}, StringSplitOptions.None)
+
+                    For col As Integer = 0 To Data.Length - 2
+
+                        newImage(col * 2, row) += Data(col)
+                        newImage(col * 2 + 1, row) += Data(col)
+
+                    Next
+                    row += 1
+                Loop
+
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Error reading input files: " & ex.Message)
+        End Try
+
+        outputfile = outputTextBox.Text & "\" & IO.Path.GetFileName(inputfile) & ".txt"
+
+        Try
+            Using sw As IO.StreamWriter = New IO.StreamWriter(IO.File.Open(outputfile, IO.FileMode.Create))
+
+                'write header
+                For row As Integer = 0 To header.Length - 1
+                    sw.WriteLine(header(row))
+                Next
+
+                For row As Integer = 0 To newrow - 1
+
+                    Dim sb As New StringBuilder()
+
+                    For col As Integer = 0 To newcolumn - 1
+                        sb.Append(String.Format("{0}" + vbTab, newImage(col, row)))
+                    Next
+
+                    'Remove the last tab from each line
+                    sb.Length -= 1
+                    sw.WriteLine(sb.ToString())
+                Next
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Error writing output file: " & ex.Message)
+            Exit Sub
+        End Try
+        RunButton.Enabled = True
+    End Sub
+
+    Private Sub InputTextBox_DragEnter(sender As Object, e As DragEventArgs) Handles inputTextBox.DragEnter
+        e.Effect = DragDropEffects.All
+        Dim file As String = e.Data.GetData(DataFormats.FileDrop)(0)
+
+        inputTextBox.Text = file
+
+        outputTextBox.Text = IO.Path.GetDirectoryName(file) & "\" & "Result"
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SetVersionInfo()
+    End Sub
+End Class
