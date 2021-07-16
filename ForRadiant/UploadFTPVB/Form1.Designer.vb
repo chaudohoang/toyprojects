81f674290136b24438aa57ecdc940b6a0a42ed22
Chau Do Hoang <hoangchaudo89@gmail.com>
2021-07-16 17:50:57 +0700

    new app

---
+++ ForRadiant/UploadFTPVB/Form1.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtHost = New System.Windows.Forms.TextBox()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnUpload = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtSource = New System.Windows.Forms.ComboBox()
+        Me.txtDest = New System.Windows.Forms.ComboBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtLog = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Host"
+        '
+        'txtHost
+        '
+        Me.txtHost.Location = New System.Drawing.Point(78, 6)
+        Me.txtHost.Name = "txtHost"
+        Me.txtHost.Size = New System.Drawing.Size(228, 20)
+        Me.txtHost.TabIndex = 1
+        Me.txtHost.Text = "127.0.0.1"
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(78, 32)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(228, 20)
+        Me.txtUsername.TabIndex = 3
+        Me.txtUsername.Text = "admin"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 35)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Username"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(78, 58)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(228, 20)
+        Me.txtPassword.TabIndex = 5
+        Me.txtPassword.Text = "admin"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 61)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Password"
+        '
+        'btnUpload
+        '
+        Me.btnUpload.Location = New System.Drawing.Point(312, 6)
+        Me.btnUpload.Name = "btnUpload"
+        Me.btnUpload.Size = New System.Drawing.Size(145, 73)
+        Me.btnUpload.TabIndex = 6
+        Me.btnUpload.Text = "Upload"
+        Me.btnUpload.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 115)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(60, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Destination"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 88)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(41, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Source"
+        '
+        'txtSource
+        '
+        Me.txtSource.FormattingEnabled = True
+        Me.txtSource.Items.AddRange(New Object() {"Current Folder"})
+        Me.txtSource.Location = New System.Drawing.Point(78, 85)
+        Me.txtSource.Name = "txtSource"
+        Me.txtSource.Size = New System.Drawing.Size(379, 21)
+        Me.txtSource.TabIndex = 9
+        Me.txtSource.Text = "Current Folder"
+        '
+        'txtDest
+        '
+        Me.txtDest.FormattingEnabled = True
+        Me.txtDest.Items.AddRange(New Object() {"HN_DATA\POCB\TEST\SUBFOLDER\"})
+        Me.txtDest.Location = New System.Drawing.Point(78, 112)
+        Me.txtDest.Name = "txtDest"
+        Me.txtDest.Size = New System.Drawing.Size(379, 21)
+        Me.txtDest.TabIndex = 10
+        Me.txtDest.Text = "HN_DATA\POCB\TEST\SUBFOLDER\"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 142)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(25, 13)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Log"
+        '
+        'txtLog
+        '
+        Me.txtLog.Location = New System.Drawing.Point(15, 166)
+        Me.txtLog.Multiline = True
+        Me.txtLog.Name = "txtLog"
+        Me.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtLog.Size = New System.Drawing.Size(442, 239)
+        Me.txtLog.TabIndex = 12
+        Me.txtLog.WordWrap = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(470, 417)
+        Me.Controls.Add(Me.txtLog)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtDest)
+        Me.Controls.Add(Me.txtSource)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnUpload)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtHost)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "UploadFTP"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtHost As TextBox
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnUpload As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtSource As ComboBox
+    Friend WithEvents txtDest As ComboBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtLog As TextBox
+End Class
---
+++ ForRadiant/UploadFTPVB/Form1.vb
@@ -0,0 +1,120 @@
+﻿Imports System.IO
+Imports System.Reflection
+Public Class Form1
+    Private Sub btnUpload_Click(sender As Object, e As EventArgs) Handles btnUpload.Click
+        Dim apppath As String = System.Reflection.Assembly.GetExecutingAssembly().Location
+        Dim appdir As String = Path.GetDirectoryName(apppath)
+
+        Dim uploadlist As New List(Of String)
+        Dim subfolder As String
+        Dim destfolder As String
+        Dim destfile As String
+        If txtSource.Text = "Current Folder" Then
+            uploadlist = GetFilesRecursive(appdir)
+            For Each sourcefile In uploadlist
+                subfolder = Path.GetDirectoryName(sourcefile).Replace(appdir, "").Replace("\", "")
+                destfolder = "ftp://" & txtHost.Text & "/" & txtDest.Text & subfolder
+                If subfolder = "" Then
+                    destfile = destfolder & Path.GetFileName(sourcefile)
+                Else
+                    destfile = destfolder & "\" & Path.GetFileName(sourcefile)
+                End If
+                CreateFTPFolder(txtDest.Text & subfolder)
+                UploadFTP(destfile, sourcefile)
+            Next
+        Else
+            uploadlist = GetFilesRecursive(txtSource.Text)
+            For Each sourcefile In uploadlist
+                subfolder = Path.GetDirectoryName(sourcefile).Replace(txtSource.Text, "").Replace("\", "")
+                destfolder = "ftp://" & txtHost.Text & "/" & txtDest.Text & subfolder
+                If subfolder = "" Then
+                    destfile = destfolder & Path.GetFileName(sourcefile)
+                Else
+                    destfile = destfolder & "\" & Path.GetFileName(sourcefile)
+                End If
+                CreateFTPFolder(txtDest.Text & subfolder)
+                UploadFTP(destfile, sourcefile)
+            Next
+        End If
+        ' Loop through and display each path.
+
+
+    End Sub
+
+    Public Shared Function GetFilesRecursive(ByVal initial As String) As List(Of String)
+        ' This list stores the results.
+        Dim result As New List(Of String)
+
+        ' This stack stores the directories to process.
+        Dim stack As New Stack(Of String)
+
+        ' Add the initial directory
+        stack.Push(initial)
+
+        ' Continue processing for each stacked directory
+        Do While (stack.Count > 0)
+            ' Get top directory string
+            Dim dir As String = stack.Pop
+            Try
+                ' Add all immediate file paths
+                result.AddRange(Directory.GetFiles(dir, "*.*"))
+
+                ' Loop through all subdirectories and add them to the stack.
+                Dim directoryName As String
+                For Each directoryName In Directory.GetDirectories(dir)
+                    stack.Push(directoryName)
+                Next
+
+            Catch ex As Exception
+            End Try
+        Loop
+
+        ' Return the list
+        Return result
+    End Function
+
+    Private Sub CreateFTPFolder(path As String)
+        Dim RequestFolderCreate As Net.FtpWebRequest = CType(System.Net.FtpWebRequest.Create("ftp://" & txtHost.Text & "/" & path), System.Net.FtpWebRequest)
+        RequestFolderCreate.Credentials = New System.Net.NetworkCredential(txtUsername.Text, txtPassword.Text)
+        RequestFolderCreate.Method = System.Net.WebRequestMethods.Ftp.MakeDirectory
+
+        Try
+            Using response As System.Net.FtpWebResponse = DirectCast(RequestFolderCreate.GetResponse(), System.Net.FtpWebResponse)
+
+            End Using
+
+        Catch ex As System.Net.WebException
+
+        End Try
+    End Sub
+
+    Public Sub UploadFTP(ByVal _UploadPath As String, ByVal _FileName As String)
+        Dim _FileInfo As New System.IO.FileInfo(_FileName)
+        Dim _FtpWebRequest As System.Net.FtpWebRequest = CType(System.Net.FtpWebRequest.Create(New Uri(_UploadPath)), System.Net.FtpWebRequest)
+        _FtpWebRequest.Credentials = New Net.NetworkCredential(txtUsername.Text, txtPassword.Text)
+        _FtpWebRequest.KeepAlive = False
+        _FtpWebRequest.Timeout = 200000
+        _FtpWebRequest.Method = System.Net.WebRequestMethods.Ftp.UploadFile
+        _FtpWebRequest.UseBinary = True
+        _FtpWebRequest.ContentLength = _FileInfo.Length
+        Dim buffLength As Integer = 2048
+        Dim buff(buffLength - 1) As Byte
+        Dim _FileStream As System.IO.FileStream = _FileInfo.OpenRead()
+        Try
+            Dim _Stream As System.IO.Stream = _FtpWebRequest.GetRequestStream()
+            Dim contentLen As Integer = _FileStream.Read(buff, 0, buffLength)
+            Do While contentLen <> 0
+                _Stream.Write(buff, 0, contentLen)
+                contentLen = _FileStream.Read(buff, 0, buffLength)
+            Loop
+            _Stream.Close()
+            _Stream.Dispose()
+            _FileStream.Close()
+            _FileStream.Dispose()
+            txtLog.Text += "Uploaded " + _FileName + " To: " + _UploadPath + Environment.NewLine
+        Catch ex As Exception
+            txtLog.Text += "Upload Error: " + ex.Message
+        End Try
+    End Sub
+
+End Class
