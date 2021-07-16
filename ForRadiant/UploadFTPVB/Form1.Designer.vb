bb08ea1265e9e0fcb9717219d4802117c036b0df
Chau Do Hoang <hoangchaudo89@gmail.com>
2021-07-16 23:55:53 +0700

    etc

--- ForRadiant/UploadFTPVB/Form1.Designer.vb
+++ ForRadiant/UploadFTPVB/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Label1 = New System.Windows.Forms.Label()
         Me.txtHost = New System.Windows.Forms.TextBox()
         Me.txtUsername = New System.Windows.Forms.TextBox()
@@ -173,6 +174,7 @@ Partial Class Form1
         Me.Controls.Add(Me.txtHost)
         Me.Controls.Add(Me.Label1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.Text = "UploadFTP"
