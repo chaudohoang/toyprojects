4ceea0e2ad6a7b525fbf922af90893aa66182fa6
Chau Do Hoang <hoangchaudo89@gmail.com>
2021-09-06 12:59:58 +0700

    new app

---
+++ ForRadiant/StartCopyWhizFixtureToCamera/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartCopyWhizFixtureToCamera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process1 = new Process();
+            process1.StartInfo.FileName = @"\\127.0.0.1\Program\RVS\Tools\CopyWhiz\CopyWhizCopy.exe";
+            process1.StartInfo.Arguments = @"\\127.0.0.1\Program\RVS\Tools\CopyWhiz\FromFixture_CopyToDooone1234.czml";
+            if (File.Exists(process1.StartInfo.FileName) && File.Exists(process1.StartInfo.Arguments) && Directory.Exists(@"\\192.168.1.11\Program") && Directory.Exists(@"\\192.168.2.12\Program") && Directory.Exists(@"\\192.168.3.13\Program") && Directory.Exists(@"\\192.168.4.14\Program"))
+            {
+                process1.Start();
+            }
+
+            Process process2 = new Process();
+            process2.StartInfo.FileName = @"\\127.0.0.1\Program\RVS\Tools\CopyWhiz\CopyWhizCopy.exe";
+            process2.StartInfo.Arguments = @"\\127.0.0.1\Program\RVS\Tools\CopyWhiz\FromFixture_CopyToGooil1234.czml";
+            if ( File.Exists(process2.StartInfo.FileName) && File.Exists(process2.StartInfo.Arguments) && Directory.Exists(@"\\192.168.0.50\Program") && Directory.Exists(@"\\192.168.0.51\Program") && Directory.Exists(@"\\192.168.0.52\Program") && Directory.Exists(@"\\192.168.0.53\Program"))
+            {
+                process2.Start();
+            }
+
+        }
+    }
+}
---
+++ ForRadiant/StartTightVNC/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartTightVNC
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (File.Exists(@"D:\Program\RVS\Tools\Installer\TightVNCDProgram.exe"))
+            {
+                ExecuteAsAdmin(@"D:\Program\RVS\Tools\Installer\TightVNCDProgram.exe");
+            }
+            else if (File.Exists(@"C:\Program\RVS\Tools\Installer\TightVNCCProgram.exe"))
+            {
+                ExecuteAsAdmin(@"C:\Program\RVS\Tools\Installer\TightVNCCProgram.exe");
+            }
+        }
+
+        public static void ExecuteAsAdmin(string fileName)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = fileName;
+            proc.StartInfo.UseShellExecute = true;
+            proc.StartInfo.Verb = "runas";
+            proc.Start();
+        }
+
+    }
+}
