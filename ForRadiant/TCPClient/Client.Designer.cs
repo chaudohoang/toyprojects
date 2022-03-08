b1dc6840a3e5a5f16d437ffcfdece805e206b43f
Chau Do Hoang <hoangchaudo89@gmail.com>
2022-03-08 16:21:46 +0700

    new app

---
+++ ForRadiant/TCPClient/Client.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace TCPClient
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Client));
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxIP = new System.Windows.Forms.ComboBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.cbxMessage = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server : ";
+            // 
+            // cbxIP
+            // 
+            this.cbxIP.FormattingEnabled = true;
+            this.cbxIP.Items.AddRange(new object[] {
+            "Gooil",
+            "Dooone1/Donga1",
+            "Dooone2/Donga2",
+            "Dooone3/Donga3",
+            "Dooone4/Donga4",
+            "127.0.0.1:9000",
+            "127.0.0.1:9001",
+            "127.0.0.1:9002",
+            "127.0.0.1:9003",
+            "127.0.0.1:9004"});
+            this.cbxIP.Location = new System.Drawing.Point(77, 63);
+            this.cbxIP.Name = "cbxIP";
+            this.cbxIP.Size = new System.Drawing.Size(237, 21);
+            this.cbxIP.TabIndex = 1;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(77, 90);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLog.Size = new System.Drawing.Size(237, 57);
+            this.txtLog.TabIndex = 3;
+            this.txtLog.TextChanged += new System.EventHandler(this.txtLog_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Log : ";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(239, 33);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // cbxMessage
+            // 
+            this.cbxMessage.FormattingEnabled = true;
+            this.cbxMessage.Items.AddRange(new object[] {
+            "Restart TrueTest",
+            "Open Notepad"});
+            this.cbxMessage.Location = new System.Drawing.Point(77, 6);
+            this.cbxMessage.Name = "cbxMessage";
+            this.cbxMessage.Size = new System.Drawing.Size(237, 21);
+            this.cbxMessage.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Message : ";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(158, 33);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(77, 33);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(75, 23);
+            this.btnRestart.TabIndex = 6;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Client";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.restartToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(111, 70);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 158);
+            this.Controls.Add(this.cbxMessage);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.cbxIP);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Client";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Client_Load);
+            this.Resize += new System.EventHandler(this.Client_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxIP;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox cbxMessage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ ForRadiant/TCPClient/Client.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SuperSimpleTcp;
+using System.Diagnostics;
+
+namespace TCPClient
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+
+        private void SetVersionInfo()
+        {
+            Version versionInfo = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            DateTime startDate = new DateTime(2000, 1, 1);
+            int diffDays = versionInfo.Build;
+            DateTime computedDate = startDate.AddDays(diffDays);
+            string lastBuilt = computedDate.ToShortDateString();
+            //this.Text = string.Format("{0} - {1} ({2})",
+            //            this.Text, versionInfo.ToString(), lastBuilt);
+            this.Text = string.Format("{0} - {1}",
+                        this.Text, versionInfo.ToString());
+        }
+
+        SimpleTcpClient client;
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {            
+            try
+            {
+                if (cbxIP.Text == "Gooil")
+                {
+                    client = new SimpleTcpClient("192.168.0.50:9000");
+                }
+                else if (cbxIP.Text == "Dooone1")
+                {
+                    client = new SimpleTcpClient("192.168.1.1:9000");
+                }
+                else if (cbxIP.Text == "Dooone2")
+                {
+                    client = new SimpleTcpClient("192.168.2.2:9000");
+                }
+                else if (cbxIP.Text == "Dooone3")
+                {
+                    client = new SimpleTcpClient("192.168.3.3:9000");
+                }
+                else if (cbxIP.Text == "Dooone4")
+                {
+                    client = new SimpleTcpClient("192.168.4.4:9000");
+                }
+                else client = new SimpleTcpClient(cbxIP.Text);
+                client.Keepalive.EnableTcpKeepAlives = true;
+                client.Keepalive.TcpKeepAliveInterval = 5;
+                client.Keepalive.TcpKeepAliveTime = 5;
+                client.Keepalive.TcpKeepAliveRetryCount = 5;
+                client.Events.Connected += Events_Connected;
+                client.Events.DataReceived += Events_DataReceived;
+                client.Events.Disconnected += Events_Disconnected;
+                client.Connect();
+                btnConnect.Enabled = false;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void Events_Disconnected(object sender, SuperSimpleTcp.ConnectionEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                txtLog.Text += $"Server disconnected.{Environment.NewLine}";
+                btnConnect.Enabled = true;
+            });
+        }
+
+        private void Events_DataReceived(object sender, SuperSimpleTcp.DataReceivedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                txtLog.Text += $"Server : {Encoding.UTF8.GetString(e.Data)}{Environment.NewLine}";
+            });
+            if (Encoding.UTF8.GetString(e.Data) == "Open Notepad")
+            {
+                try
+                {
+                    Process.Start("notepad.exe");
+                }
+                catch (Exception)
+                {
+
+                }
+                
+            }
+            else if (Encoding.UTF8.GetString(e.Data) == "Restart TrueTest")
+            {
+                try
+                {
+                    foreach (var process in Process.GetProcessesByName("TrueTest"))
+                    {
+                        process.Kill();
+                    }
+                    Process.Start("TrueTest.exe");
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+
+        private void Events_Connected(object sender, SuperSimpleTcp.ConnectionEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                txtLog.Text += $"Server connected.{Environment.NewLine}";
+            });
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (client != null && client.IsConnected)
+            {
+                if (!string.IsNullOrEmpty(cbxMessage.Text))
+                {
+                    client.Send(cbxMessage.Text);
+                    txtLog.Text += $"Me : {cbxMessage.Text}{Environment.NewLine}";
+                    cbxMessage.Text = string.Empty;
+                }
+            }
+        }
+
+        private void txtLog_TextChanged(object sender, EventArgs e)
+        {
+            txtLog.SelectionStart = txtLog.Text.Length;
+            txtLog.ScrollToCaret();
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+            Environment.Exit(0);
+        }
+
+        private void Client_Load(object sender, EventArgs e)
+        {
+            SetVersionInfo();
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Show();
+            Activate();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+            Environment.Exit(0);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Client_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            Activate();
+            this.WindowState = FormWindowState.Normal;
+        }
+    }
+}
