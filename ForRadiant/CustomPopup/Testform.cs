2130f3d1117e1b976b4c28e123b9b1096a6db04b
Chau Do Hoang <hoangchaudo89@gmail.com>
2022-04-23 03:17:39 +0700

    update

--- ForRadiant/CustomPopup/Testform.cs
+++ ForRadiant/CustomPopup/Testform.cs
@@ -41,5 +41,11 @@ namespace CustomPopup
         {
             SetVersionInfo();
         }
-    }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CustomForm customform = new CustomForm();
+            customform.Show();
+        }
+	}
 }
