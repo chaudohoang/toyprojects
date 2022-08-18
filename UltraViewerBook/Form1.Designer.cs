cef605899b84a9bc27ef53421f4ea70d6f36cae1
Chau Do Hoang <hoangchaudo89@gmail.com>
2022-08-18 18:17:30 +0700

    update

--- UltraViewerBook/Form1.Designer.cs
+++ UltraViewerBook/Form1.Designer.cs
@@ -73,7 +73,9 @@ namespace UltraViewerBook
 			this.Controls.Add(this.btnRemoteControl);
 			this.Controls.Add(this.dataGridView1);
 			this.Controls.Add(this.btnSave);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
 			this.Name = "Form1";
 			this.Text = "UltraViewerBook";
 			this.Load += new System.EventHandler(this.Form1_Load);
