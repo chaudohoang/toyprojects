d390c40a929c7165327ed5ff4fba749502453a65
dh.chau <dh.chau@radiantvs.com>
2022-11-18 07:41:18 +0700

    new app

---
+++ ForRadiant/RestartTTDove2p0/MainForm.Designer.vb
@@ -0,0 +1,253 @@
+﻿
+Imports System
+
+Namespace RestartTT
+	Partial Class MainForm
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <paramname="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.components = New System.ComponentModel.Container()
+			Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+			Me.notifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+			Me.contextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+			Me.exit2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+			Me.menuStrip1 = New System.Windows.Forms.MenuStrip()
+			Me.commandToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+			Me.exitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+			Me.settingsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+			Me.startMinimizedToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+			Me.minimizedToTrayToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+			Me.helpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+			Me.aboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+			Me.txtRunCount = New System.Windows.Forms.TextBox()
+			Me.label1 = New System.Windows.Forms.Label()
+			Me.label2 = New System.Windows.Forms.Label()
+			Me.cmdSaveSettings = New System.Windows.Forms.LinkLabel()
+			Me.lblRunCount = New System.Windows.Forms.Label()
+			Me.chkResetCount = New System.Windows.Forms.CheckBox()
+			Me.txtWait = New System.Windows.Forms.TextBox()
+			Me.Label3 = New System.Windows.Forms.Label()
+			Me.contextMenuStrip1.SuspendLayout()
+			Me.menuStrip1.SuspendLayout()
+			Me.SuspendLayout()
+			'
+			'notifyIcon1
+			'
+			Me.notifyIcon1.ContextMenuStrip = Me.contextMenuStrip1
+			Me.notifyIcon1.Icon = CType(resources.GetObject("notifyIcon1.Icon"), System.Drawing.Icon)
+			Me.notifyIcon1.Text = "RestartTTDove2p0"
+			Me.notifyIcon1.Visible = True
+			'
+			'contextMenuStrip1
+			'
+			Me.contextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.exit2ToolStripMenuItem})
+			Me.contextMenuStrip1.Name = "contextMenuStrip1"
+			Me.contextMenuStrip1.Size = New System.Drawing.Size(94, 26)
+			'
+			'exit2ToolStripMenuItem
+			'
+			Me.exit2ToolStripMenuItem.Name = "exit2ToolStripMenuItem"
+			Me.exit2ToolStripMenuItem.Size = New System.Drawing.Size(93, 22)
+			Me.exit2ToolStripMenuItem.Text = "Exit"
+			'
+			'menuStrip1
+			'
+			Me.menuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.commandToolStripMenuItem, Me.settingsToolStripMenuItem, Me.helpToolStripMenuItem})
+			Me.menuStrip1.Location = New System.Drawing.Point(0, 0)
+			Me.menuStrip1.Name = "menuStrip1"
+			Me.menuStrip1.Size = New System.Drawing.Size(349, 24)
+			Me.menuStrip1.TabIndex = 1
+			Me.menuStrip1.Text = "menuStrip1"
+			'
+			'commandToolStripMenuItem
+			'
+			Me.commandToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.exitToolStripMenuItem})
+			Me.commandToolStripMenuItem.Name = "commandToolStripMenuItem"
+			Me.commandToolStripMenuItem.Size = New System.Drawing.Size(76, 20)
+			Me.commandToolStripMenuItem.Text = "Command"
+			'
+			'exitToolStripMenuItem
+			'
+			Me.exitToolStripMenuItem.Name = "exitToolStripMenuItem"
+			Me.exitToolStripMenuItem.Size = New System.Drawing.Size(93, 22)
+			Me.exitToolStripMenuItem.Text = "Exit"
+			'
+			'settingsToolStripMenuItem
+			'
+			Me.settingsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.startMinimizedToolStripMenuItem, Me.minimizedToTrayToolStripMenuItem})
+			Me.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem"
+			Me.settingsToolStripMenuItem.Size = New System.Drawing.Size(61, 20)
+			Me.settingsToolStripMenuItem.Text = "Settings"
+			'
+			'startMinimizedToolStripMenuItem
+			'
+			Me.startMinimizedToolStripMenuItem.CheckOnClick = True
+			Me.startMinimizedToolStripMenuItem.Name = "startMinimizedToolStripMenuItem"
+			Me.startMinimizedToolStripMenuItem.Size = New System.Drawing.Size(226, 22)
+			Me.startMinimizedToolStripMenuItem.Text = "Start Minimized"
+			'
+			'minimizedToTrayToolStripMenuItem
+			'
+			Me.minimizedToTrayToolStripMenuItem.CheckOnClick = True
+			Me.minimizedToTrayToolStripMenuItem.Name = "minimizedToTrayToolStripMenuItem"
+			Me.minimizedToTrayToolStripMenuItem.Size = New System.Drawing.Size(226, 22)
+			Me.minimizedToTrayToolStripMenuItem.Text = "Minimized Hide Taskbar Icon"
+			'
+			'helpToolStripMenuItem
+			'
+			Me.helpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.aboutToolStripMenuItem})
+			Me.helpToolStripMenuItem.Name = "helpToolStripMenuItem"
+			Me.helpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+			Me.helpToolStripMenuItem.Text = "Help"
+			'
+			'aboutToolStripMenuItem
+			'
+			Me.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem"
+			Me.aboutToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+			Me.aboutToolStripMenuItem.Text = "About"
+			'
+			'txtRunCount
+			'
+			Me.txtRunCount.Location = New System.Drawing.Point(211, 21)
+			Me.txtRunCount.Name = "txtRunCount"
+			Me.txtRunCount.Size = New System.Drawing.Size(43, 20)
+			Me.txtRunCount.TabIndex = 3
+			Me.txtRunCount.Text = "100"
+			'
+			'label1
+			'
+			Me.label1.AutoSize = True
+			Me.label1.Location = New System.Drawing.Point(12, 24)
+			Me.label1.Name = "label1"
+			Me.label1.Size = New System.Drawing.Size(193, 13)
+			Me.label1.TabIndex = 4
+			Me.label1.Text = "Auto Restart TT Every Number of Runs"
+			'
+			'label2
+			'
+			Me.label2.AutoSize = True
+			Me.label2.ForeColor = System.Drawing.Color.Purple
+			Me.label2.Location = New System.Drawing.Point(13, 94)
+			Me.label2.Name = "label2"
+			Me.label2.Size = New System.Drawing.Size(67, 13)
+			Me.label2.TabIndex = 11
+			Me.label2.Text = "Run Count : "
+			'
+			'cmdSaveSettings
+			'
+			Me.cmdSaveSettings.AutoSize = True
+			Me.cmdSaveSettings.Location = New System.Drawing.Point(12, 71)
+			Me.cmdSaveSettings.Name = "cmdSaveSettings"
+			Me.cmdSaveSettings.Size = New System.Drawing.Size(68, 13)
+			Me.cmdSaveSettings.TabIndex = 13
+			Me.cmdSaveSettings.TabStop = True
+			Me.cmdSaveSettings.Text = "Save Setting"
+			'
+			'lblRunCount
+			'
+			Me.lblRunCount.AutoSize = True
+			Me.lblRunCount.ForeColor = System.Drawing.Color.Purple
+			Me.lblRunCount.Location = New System.Drawing.Point(86, 94)
+			Me.lblRunCount.Name = "lblRunCount"
+			Me.lblRunCount.Size = New System.Drawing.Size(13, 13)
+			Me.lblRunCount.TabIndex = 15
+			Me.lblRunCount.Text = "0"
+			'
+			'chkResetCount
+			'
+			Me.chkResetCount.AutoSize = True
+			Me.chkResetCount.Location = New System.Drawing.Point(86, 71)
+			Me.chkResetCount.Name = "chkResetCount"
+			Me.chkResetCount.Size = New System.Drawing.Size(130, 17)
+			Me.chkResetCount.TabIndex = 16
+			Me.chkResetCount.Text = "Reset Count On Save"
+			Me.chkResetCount.UseVisualStyleBackColor = True
+			'
+			'txtWait
+			'
+			Me.txtWait.Location = New System.Drawing.Point(211, 46)
+			Me.txtWait.Name = "txtWait"
+			Me.txtWait.Size = New System.Drawing.Size(43, 20)
+			Me.txtWait.TabIndex = 17
+			Me.txtWait.Text = "3"
+			'
+			'Label3
+			'
+			Me.Label3.AutoSize = True
+			Me.Label3.Location = New System.Drawing.Point(12, 49)
+			Me.Label3.Name = "Label3"
+			Me.Label3.Size = New System.Drawing.Size(113, 13)
+			Me.Label3.TabIndex = 18
+			Me.Label3.Text = "Wait before Restart (s)"
+			'
+			'MainForm
+			'
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(349, 118)
+			Me.Controls.Add(Me.txtWait)
+			Me.Controls.Add(Me.Label3)
+			Me.Controls.Add(Me.chkResetCount)
+			Me.Controls.Add(Me.lblRunCount)
+			Me.Controls.Add(Me.cmdSaveSettings)
+			Me.Controls.Add(Me.label2)
+			Me.Controls.Add(Me.txtRunCount)
+			Me.Controls.Add(Me.label1)
+			Me.Controls.Add(Me.menuStrip1)
+			Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+			Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+			Me.MainMenuStrip = Me.menuStrip1
+			Me.MaximizeBox = False
+			Me.Name = "MainForm"
+			Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+			Me.Text = "Restart TT Dove2p0"
+			Me.contextMenuStrip1.ResumeLayout(False)
+			Me.menuStrip1.ResumeLayout(False)
+			Me.menuStrip1.PerformLayout()
+			Me.ResumeLayout(False)
+			Me.PerformLayout()
+
+		End Sub
+
+#End Region
+		Friend WithEvents notifyIcon1 As Windows.Forms.NotifyIcon
+		Friend WithEvents contextMenuStrip1 As Windows.Forms.ContextMenuStrip
+		Friend WithEvents exit2ToolStripMenuItem As Windows.Forms.ToolStripMenuItem
+		Friend WithEvents menuStrip1 As Windows.Forms.MenuStrip
+		Friend WithEvents commandToolStripMenuItem As Windows.Forms.ToolStripMenuItem
+		Friend WithEvents exitToolStripMenuItem As Windows.Forms.ToolStripMenuItem
+		Friend WithEvents settingsToolStripMenuItem As Windows.Forms.ToolStripMenuItem
+		Friend WithEvents minimizedToTrayToolStripMenuItem As Windows.Forms.ToolStripMenuItem
+		Friend WithEvents helpToolStripMenuItem As Windows.Forms.ToolStripMenuItem
+		Friend WithEvents aboutToolStripMenuItem As Windows.Forms.ToolStripMenuItem
+		Private WithEvents txtRunCount As Windows.Forms.TextBox
+		Private WithEvents label1 As Windows.Forms.Label
+		Private WithEvents label2 As Windows.Forms.Label
+		Friend WithEvents cmdSaveSettings As Windows.Forms.LinkLabel
+		Private WithEvents lblRunCount As Windows.Forms.Label
+		Friend WithEvents chkResetCount As Windows.Forms.CheckBox
+		Friend WithEvents startMinimizedToolStripMenuItem As Windows.Forms.ToolStripMenuItem
+		Private WithEvents txtWait As Windows.Forms.TextBox
+		Private WithEvents Label3 As Windows.Forms.Label
+	End Class
+End Namespace
---
+++ ForRadiant/RestartTTDove2p0/MainForm.vb
@@ -0,0 +1,387 @@
+﻿Imports System
+Imports System.Windows.Forms
+Imports System.Runtime.InteropServices
+Imports System.Reflection
+Imports System.IO
+Imports System.Collections.Generic
+Imports System.Diagnostics
+Imports Microsoft.VisualBasic
+Imports System.Linq
+Imports System.Drawing
+Imports System.Threading
+Imports System.Threading.Tasks
+
+Namespace RestartTT
+	Partial Public Class MainForm
+		Inherits Form
+		Public apppath As String
+		Public appdir As String
+		Public settingPath As String
+		Public checkLastLineTask As Tasks.Task
+		Public monitorTask As Tasks.Task
+		Public selfRestartTask As Tasks.Task
+		Public checkLastLineTasksCancellationTokenSource As New CancellationTokenSource
+		Public monitorTaskTasksCancellationTokenSource As New CancellationTokenSource
+		Public RunCount As Integer = 0
+		Public offset As Long = 0
+		Public checkingLastLine As Boolean = False
+		Private allowVisible As Boolean = True
+		Public today As String = Now.ToString("yyyyMMdd")
+		Public logPath As String = "C:\Radiant Vision Systems Data\TrueTest\AppData\" + today + " Operation Log.txt"
+		Public sw As Stopwatch
+
+		<DllImport("User32.dll")>
+		Private Shared Function GetLastInputInfo(ByRef plii As MainForm.LASTINPUTINFO) As Boolean
+		End Function
+
+		Friend Structure LASTINPUTINFO
+			Public cbSize As UInteger
+
+			Public dwTime As UInteger
+		End Structure
+
+		Public Sub New()
+			apppath = Assembly.GetExecutingAssembly().Location
+			appdir = Path.GetDirectoryName(apppath)
+			settingPath = Path.Combine(appdir, "setting.txt")
+			InitializeComponent()
+		End Sub
+
+		Protected Overrides Sub WndProc(ByRef m As Message)
+			If m.Msg = NativeMethods.WM_SHOWME Then
+				ShowMe()
+			End If
+			MyBase.WndProc(m)
+		End Sub
+		Private Sub ShowMe()
+			Show()
+			If WindowState = FormWindowState.Minimized Then
+				WindowState = FormWindowState.Normal
+			End If
+			' get our current "TopMost" value (ours will always be false though)
+			Dim top = TopMost
+			' make our form jump to the top of everything
+			TopMost = True
+			' set it back to whatever it was
+			TopMost = top
+		End Sub
+
+		Private Sub SetVersionInfo()
+			Dim versionInfo As Version = Assembly.GetExecutingAssembly().GetName().Version
+			Dim startDate As Date = New DateTime(2000, 1, 1)
+			Dim diffDays = versionInfo.Build
+			Dim computedDate = startDate.AddDays(diffDays)
+			Dim lastBuilt As String = computedDate.ToShortDateString()
+			'this.Text = string.Format("{0} - {1} ({2})",
+			'            this.Text, versionInfo.ToString(), lastBuilt);
+			Text = String.Format("{0} - {1}", Text, versionInfo.ToString())
+		End Sub
+
+		Protected Overrides Sub SetVisibleCore(ByVal value As Boolean)
+			If Not allowVisible Then
+				value = False
+				If Not IsHandleCreated Then CreateHandle()
+			End If
+			MyBase.SetVisibleCore(value)
+		End Sub
+		Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+			SetVersionInfo()
+			LoadSettings()
+			If startMinimizedToolStripMenuItem.Checked = True Then
+				Me.WindowState = FormWindowState.Minimized
+			End If
+			StartMonitor()
+			selfRestartTask = New Tasks.Task(New Action(Sub() CheckForSelfRestart()))
+			selfRestartTask.Start()
+		End Sub
+		Private Sub CheckForSelfRestart()
+			Dim startTime As DateTime = DateTime.Now
+			Dim tomorrow As DateTime
+
+			tomorrow = startTime + New TimeSpan(1, 0, 0, 0, 0)
+			Dim year = tomorrow.Year
+			Dim month = tomorrow.Month
+			Dim day = tomorrow.Day
+			Dim endTime = New DateTime(year, month, day, 0, 0, 0)
+			Dim duration = endTime - startTime
+			Dim durationInSeconds = duration.TotalSeconds
+			sw = New Stopwatch
+			sw.Start()
+			While sw.ElapsedMilliseconds <= durationInSeconds * 1000
+				System.Threading.Thread.Sleep(100)
+			End While
+			Application.Restart()
+		End Sub
+
+		Private Sub aboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles aboutToolStripMenuItem.Click
+			MessageBox.Show("dh.chau@radiantvs.com")
+		End Sub
+
+		Private Sub exitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles exitToolStripMenuItem.Click
+			Application.Exit()
+		End Sub
+
+		Private Sub exit2ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles exit2ToolStripMenuItem.Click
+			Application.Exit()
+		End Sub
+
+		Private Sub MainForm_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+			If minimizedToTrayToolStripMenuItem.Checked = True AndAlso WindowState = FormWindowState.Minimized Then
+				Hide()
+				ShowInTaskbar = False
+				notifyIcon1.BalloonTipText = "RestartTTDove2p0 still running and minimized to tray"
+				notifyIcon1.ShowBalloonTip(100)
+			End If
+		End Sub
+		Private Sub notifyIcon1_DoubleClick(sender As Object, e As EventArgs) Handles notifyIcon1.DoubleClick
+			allowVisible = True
+			Show()
+			Activate()
+			ShowInTaskbar = True
+			WindowState = FormWindowState.Normal
+		End Sub
+
+		Private Sub StartMonitor()
+
+			checkLastLineTasksCancellationTokenSource = New CancellationTokenSource
+			checkLastLineTask = New Tasks.Task(New Action(Sub() GetLastLine()), checkLastLineTasksCancellationTokenSource.Token)
+			checkLastLineTask.Start()
+			monitorTaskTasksCancellationTokenSource = New CancellationTokenSource
+			monitorTask = New Tasks.Task(New Action(Sub() ReadFromFile()), monitorTaskTasksCancellationTokenSource.Token)
+			monitorTask.Start()
+
+		End Sub
+		Private Sub ReCheckLastLine()
+			If checkLastLineTasksCancellationTokenSource IsNot Nothing Then
+				checkLastLineTasksCancellationTokenSource.Cancel()
+			End If
+			If checkLastLineTask IsNot Nothing Then
+				If checkLastLineTask.Status <> TaskStatus.RanToCompletion Then
+					'Wait a little longer
+					Dim sw As New Stopwatch
+					sw.Start()
+					Do Until checkLastLineTask.Status = TaskStatus.RanToCompletion
+						If checkLastLineTask.Status = TaskStatus.Canceled Then Exit Do
+						If checkLastLineTask.Status = TaskStatus.Faulted Then Exit Do
+						If sw.ElapsedMilliseconds > 1000 Then Exit Do
+					Loop
+					sw.Stop()
+				End If
+				If checkLastLineTask.IsCompleted OrElse checkLastLineTask.IsCanceled OrElse checkLastLineTask.IsFaulted Then
+					checkLastLineTask.Dispose()
+				End If
+				checkLastLineTask = Nothing
+			End If
+			If chkResetCount.Checked Then
+				RunCount = 0
+				lblRunCount.Invoke(Sub()
+									   lblRunCount.Text = RunCount.ToString
+								   End Sub)
+			End If
+			checkLastLineTasksCancellationTokenSource = New CancellationTokenSource
+			checkLastLineTask = New Tasks.Task(New Action(Sub() GetLastLine()), checkLastLineTasksCancellationTokenSource.Token)
+			checkLastLineTask.Start()
+		End Sub
+		Private Sub GetLastLine()
+			checkingLastLine = True
+			If checkLastLineTasksCancellationTokenSource.IsCancellationRequested Then
+				Exit Sub
+			End If
+			If Not File.Exists(logPath) Then
+				File.WriteAllText(logPath, "")
+			End If
+			Dim fsw As New FileSystemWatcher
+			fsw.Path = "C:\Radiant Vision Systems Data\TrueTest\AppData"
+			fsw.Filter = "*.*"
+
+			Dim file__1 As FileStream = File.Open(logPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+
+			Dim reader As New StreamReader(file__1)
+			Do
+				If checkLastLineTasksCancellationTokenSource.IsCancellationRequested Then
+					Exit Sub
+				End If
+				reader.ReadLine()
+				If reader.EndOfStream Then
+					offset = file__1.Position
+					checkingLastLine = False
+					Exit Sub
+				End If
+			Loop While Not reader.EndOfStream
+		End Sub
+		Private Sub ReadFromFile()
+			While checkingLastLine
+				If monitorTaskTasksCancellationTokenSource.IsCancellationRequested Then
+					Exit Sub
+				End If
+				Thread.Sleep(5)
+				If Not checkingLastLine Then
+					Exit While
+				End If
+			End While
+			RunCount = 0
+			Dim fsw As New FileSystemWatcher
+			fsw.Path = "C:\Radiant Vision Systems Data\TrueTest\AppData"
+			fsw.Filter = "*.*"
+			If monitorTaskTasksCancellationTokenSource.IsCancellationRequested Then
+				Exit Sub
+			End If
+			Dim file__1 As FileStream = File.Open(logPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+
+			Dim reader As New StreamReader(file__1)
+			While Not monitorTaskTasksCancellationTokenSource.IsCancellationRequested
+				If monitorTaskTasksCancellationTokenSource.IsCancellationRequested Then
+					Exit Sub
+				End If
+				fsw.WaitForChanged(WatcherChangeTypes.Changed)
+
+				file__1.Seek(offset, SeekOrigin.Begin)
+
+				If Not reader.EndOfStream Then
+					Do
+						If monitorTaskTasksCancellationTokenSource.IsCancellationRequested Then
+							Exit Sub
+						End If
+						Dim checkString As String = reader.ReadLine()
+						If checkString.Contains("Sent : RESULT") Then
+							RunCount += 1
+							lblRunCount.Invoke(Sub()
+												   lblRunCount.Text = RunCount.ToString
+											   End Sub)
+						End If
+						If RunCount.ToString = txtRunCount.Text Then
+							Try
+								Thread.Sleep(3000)
+								RunCount = 0
+								lblRunCount.Invoke(Sub()
+													   lblRunCount.Text = RunCount.ToString
+												   End Sub)
+								For Each process As Process In Process.GetProcessesByName("TrueTest")
+									process.Kill()
+									Process.Start("TrueTest")
+								Next
+							Catch ex As Exception
+
+							End Try
+						End If
+
+					Loop While Not reader.EndOfStream
+
+					offset = file__1.Position
+
+				End If
+			End While
+		End Sub
+		Private Sub StopMonitor()
+
+			If checkLastLineTasksCancellationTokenSource IsNot Nothing Then
+				checkLastLineTasksCancellationTokenSource.Cancel()
+			End If
+			If monitorTaskTasksCancellationTokenSource IsNot Nothing Then
+				monitorTaskTasksCancellationTokenSource.Cancel()
+			End If
+			If checkLastLineTask IsNot Nothing Then
+				If checkLastLineTask.Status <> TaskStatus.RanToCompletion Then
+					'Wait a little longer
+					Dim sw As New Stopwatch
+					sw.Start()
+					Do Until checkLastLineTask.Status = TaskStatus.RanToCompletion
+						If checkLastLineTask.Status = TaskStatus.Canceled Then Exit Do
+						If checkLastLineTask.Status = TaskStatus.Faulted Then Exit Do
+						If sw.ElapsedMilliseconds > 1000 Then Exit Do
+					Loop
+					sw.Stop()
+				End If
+				If checkLastLineTask.IsCompleted OrElse checkLastLineTask.IsCanceled OrElse checkLastLineTask.IsFaulted Then
+					checkLastLineTask.Dispose()
+				End If
+				checkLastLineTask = Nothing
+			End If
+
+			If monitorTask IsNot Nothing Then
+				If monitorTask.Status <> TaskStatus.RanToCompletion Then
+					'Wait a little longer
+					Dim sw As New Stopwatch
+					sw.Start()
+					Do Until monitorTask.Status = TaskStatus.RanToCompletion
+						If monitorTask.Status = TaskStatus.Canceled Then Exit Do
+						If monitorTask.Status = TaskStatus.Faulted Then Exit Do
+						If sw.ElapsedMilliseconds > 1000 Then Exit Do
+					Loop
+					sw.Stop()
+				End If
+				If monitorTask.IsCompleted OrElse monitorTask.IsCanceled OrElse monitorTask.IsFaulted Then
+					monitorTask.Dispose()
+				End If
+				monitorTask = Nothing
+			End If
+
+		End Sub
+
+		Private Sub cmdSaveSettings_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles cmdSaveSettings.LinkClicked
+			SaveSettings()
+			ReCheckLastLine()
+		End Sub
+		Private Sub SaveSettings()
+			Dim settings As New Dictionary(Of String, String)
+			If startMinimizedToolStripMenuItem.Checked Then
+				settings.Add("startminimized", "true")
+			Else
+				settings.Add("startminimized", "false")
+			End If
+			If minimizedToTrayToolStripMenuItem.Checked Then
+				settings.Add("minimizedtotray", "true")
+			Else
+				settings.Add("minimizedtotray", "false")
+			End If
+			settings.Add("numruns", txtRunCount.Text)
+			settings.Add("waitsec", txtWait.Text)
+			If chkResetCount.Checked = True Then
+				settings.Add("resetcountonsave", "true")
+			Else
+				settings.Add("resetcountonsave", "false")
+			End If
+			Dim settingContent As String = ""
+			Dim keys() As String = settings.Keys.ToArray
+			For Each k As String In keys
+				settingContent += k + "=" + settings(k) + Environment.NewLine
+			Next
+			Try
+				File.WriteAllText(settingPath, settingContent)
+			Catch ex As Exception
+
+			End Try
+		End Sub
+		Private Sub LoadSettings()
+			Try
+				If File.Exists(settingPath) Then
+					Dim settings() As String = File.ReadAllLines(settingPath)
+					For Each line As String In settings
+						Dim setting As String = line.Split("=")(0)
+						Dim value As String = line.Split("=")(1)
+						If setting = "startminimized" Then
+							startMinimizedToolStripMenuItem.Checked = If(value = "true", True, False)
+						ElseIf setting = "minimizedtotray" Then
+							minimizedToTrayToolStripMenuItem.Checked = If(value = "true", True, False)
+						ElseIf setting = "numruns" Then
+							txtRunCount.Text = value
+						ElseIf setting = "waitsec" Then
+							txtWait.Text = value
+						ElseIf setting = "resetcountonsave" Then
+							chkResetCount.Checked = If(value = "true", True, False)
+						End If
+					Next
+				End If
+			Catch ex As Exception
+
+			End Try
+		End Sub
+
+		Private Sub cmdStopUpload_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs)
+			StopMonitor()
+
+		End Sub
+
+	End Class
+End Namespace
