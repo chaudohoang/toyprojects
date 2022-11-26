71265cc4b134e62a779138a5967a908a833cadb1
dh.chau <dh.chau@radiantvs.com>
2022-11-26 09:33:21 +0700

    update

--- ForRadiant/SequenceCheck/My Project/Application.Designer.vb
+++ ForRadiant/SequenceCheck/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.SequenceCheck.Form1
+            Me.MainForm = Global.SequenceCheck.MainForm
         End Sub
     End Class
 End Namespace
