3d61ecead14e098db106da67ead2b7b64f09f0d3
dh.chau <dh.chau@radiantvs.com>
2022-11-26 10:47:42 +0700

    update

--- ForRadiant/SequenceCheck/My Project/AssemblyInfo.vb
+++ ForRadiant/SequenceCheck/My Project/AssemblyInfo.vb
@@ -31,7 +31,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.2")>
-<Assembly: AssemblyFileVersion("1.0.0.2")>
+<Assembly: AssemblyVersion("1.0.0.3")>
+<Assembly: AssemblyFileVersion("1.0.0.3")>
 
-<assembly: AssemblyInformationalVersion("1.0.41")>
+<assembly: AssemblyInformationalVersion("1.0.42")>
