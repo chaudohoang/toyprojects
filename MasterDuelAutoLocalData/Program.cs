3f9e26743d91e353fcdce9753c3611f97b99f425
Do Hoang Chau <dh.chau@radiantvs.com>
2024-03-04 20:29:09 +0700

    update

---
+++ MasterDuelAutoLocalData/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string rootFolder = @"E:\Games\Steam\steamapps\common\Yu-Gi-Oh!  Master Duel\LocalData"; // Update this with the actual path to your LocalData folder
+        string actualFolder = @"E:\Games\Steam\steamapps\common\Yu-Gi-Oh!  Master Duel\0000"; // Update this with the actual path to the actual "0000" folder
+        CheckAndCreateSoftLink(rootFolder, actualFolder);
+    }
+
+    static void CheckAndCreateSoftLink(string rootFolder, string actualFolder)
+    {
+        string[] subfolders = Directory.GetDirectories(rootFolder);
+
+        foreach (string folderPath in subfolders)
+        {
+            string folderName = Path.GetFileName(folderPath);
+
+            if (Directory.Exists(folderPath))
+            {
+                string subfolder0000Path = Path.Combine(folderPath, "0000");
+
+                // Check if the "0000" subfolder exists and is not a soft link
+                if (Directory.Exists(subfolder0000Path) && !IsSymbolicLink(subfolder0000Path))
+                {
+                    Console.WriteLine($"Deleting {subfolder0000Path}");
+                    Directory.Delete(subfolder0000Path, true); // Delete the folder and its contents
+                }
+
+                // Check if "0000" subfolder does not exist or it's not a soft link, then create a soft link
+                if (!Directory.Exists(subfolder0000Path) || !IsSymbolicLink(subfolder0000Path))
+                {
+                    // Create a soft link to "0000" folder
+                    CreateSymbolicLink(subfolder0000Path, actualFolder);
+                    Console.WriteLine($"Created soft link for {folderName}");
+                }
+                else
+                {
+                    Console.WriteLine($"Folder {folderName} already contains '0000' subfolder as a soft link.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{folderName} is not a directory.");
+            }
+        }
+    }
+
+
+    static bool IsSymbolicLink(string path)
+    {
+        DirectoryInfo di = new DirectoryInfo(path);
+        return di.Attributes.HasFlag(FileAttributes.ReparsePoint);
+    }
+
+    static void CreateSymbolicLink(string linkPath, string targetPath)
+    {
+        if (!File.Exists(linkPath))
+        {
+            var process = new System.Diagnostics.Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = $"/c mklink /D \"{linkPath}\" \"{targetPath}\"";
+            process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
---
+++ SteamAccountSwitch/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace SteamAccountSwitch
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSteamExePath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(198, 459);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(180, 23);
+            this.btnLogin.TabIndex = 10;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(367, 401);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dataGridView1_CellFormatting);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 459);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(180, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save Accounts";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Steam exe path";
+            // 
+            // txtSteamExePath
+            // 
+            this.txtSteamExePath.Location = new System.Drawing.Point(11, 25);
+            this.txtSteamExePath.Name = "txtSteamExePath";
+            this.txtSteamExePath.Size = new System.Drawing.Size(311, 20);
+            this.txtSteamExePath.TabIndex = 12;
+            this.txtSteamExePath.Text = "\"D:\\Games\\Steam\\steam.exe\"";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(328, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 494);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSteamExePath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Steam Account Switch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button btnLogin;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSteamExePath;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
