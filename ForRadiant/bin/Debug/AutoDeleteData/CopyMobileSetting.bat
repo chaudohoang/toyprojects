a7e6ff31022616dc788382f3ac3263be84ef43e7
dh.chau <dh.chau@radiantvs.com>
2024-03-21 14:21:44 +0700

    add mobile setting

---
+++ ForRadiant/bin/Debug/AutoDeleteData/CopyMobileSetting.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal enabledelayedexpansion
+set sourceFolder="%~dp0MobileSetting"
+set destinationFolder="C:\Radiant Vision Systems Data\TrueTest\UserData"
+xcopy /Y /S /E %sourceFolder% %destinationFolder%
+
+for /l %%i in (3,-1,1) do (
+    echo Closing in %%i seconds...
+    timeout /t 1 >nul
+)
