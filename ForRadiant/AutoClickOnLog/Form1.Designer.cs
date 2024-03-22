c9e356765941b408c607d7fe09197938c7c9b06c
dh.chau <dh.chau@radiantvs.com>
2024-03-22 22:20:38 +0700

    add

---
+++ ForRadiant/AutoClickOnLog/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace AutoClickOnLog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LogFolderTextBox = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LogStringTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ActionDelayTextBox = new System.Windows.Forms.TextBox();
+            this.ViewlogButton = new System.Windows.Forms.Button();
+            this.ClearlogButton = new System.Windows.Forms.Button();
+            this.lblLastAction = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(174, 17);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(46, 13);
+            this.lblStatus.TabIndex = 9;
+            this.lblStatus.Text = "Status : ";
+            // 
+            // StopButton
+            // 
+            this.StopButton.Enabled = false;
+            this.StopButton.Location = new System.Drawing.Point(93, 12);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 8;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Mlog Folder";
+            // 
+            // LogFolderTextBox
+            // 
+            this.LogFolderTextBox.Location = new System.Drawing.Point(99, 46);
+            this.LogFolderTextBox.Name = "LogFolderTextBox";
+            this.LogFolderTextBox.Size = new System.Drawing.Size(475, 20);
+            this.LogFolderTextBox.TabIndex = 6;
+            this.LogFolderTextBox.Text = "C:\\Users\\dh.chau\\Downloads\\Compressed\\GOOIL LOG";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(12, 12);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 5;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Log String";
+            // 
+            // LogStringTextBox
+            // 
+            this.LogStringTextBox.Location = new System.Drawing.Point(99, 80);
+            this.LogStringTextBox.Name = "LogStringTextBox";
+            this.LogStringTextBox.Size = new System.Drawing.Size(475, 20);
+            this.LogStringTextBox.TabIndex = 11;
+            this.LogStringTextBox.Text = "Inspection Complete";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Action Delay (s)";
+            // 
+            // ActionDelayTextBox
+            // 
+            this.ActionDelayTextBox.Location = new System.Drawing.Point(99, 115);
+            this.ActionDelayTextBox.Name = "ActionDelayTextBox";
+            this.ActionDelayTextBox.Size = new System.Drawing.Size(475, 20);
+            this.ActionDelayTextBox.TabIndex = 14;
+            this.ActionDelayTextBox.Text = "15";
+            // 
+            // ViewlogButton
+            // 
+            this.ViewlogButton.Location = new System.Drawing.Point(442, 12);
+            this.ViewlogButton.Name = "ViewlogButton";
+            this.ViewlogButton.Size = new System.Drawing.Size(56, 23);
+            this.ViewlogButton.TabIndex = 16;
+            this.ViewlogButton.Text = "View log";
+            this.ViewlogButton.UseVisualStyleBackColor = true;
+            this.ViewlogButton.Click += new System.EventHandler(this.ViewlogButton_Click);
+            // 
+            // ClearlogButton
+            // 
+            this.ClearlogButton.Location = new System.Drawing.Point(504, 12);
+            this.ClearlogButton.Name = "ClearlogButton";
+            this.ClearlogButton.Size = new System.Drawing.Size(70, 23);
+            this.ClearlogButton.TabIndex = 17;
+            this.ClearlogButton.Text = "Clear log";
+            this.ClearlogButton.UseVisualStyleBackColor = true;
+            this.ClearlogButton.Click += new System.EventHandler(this.ClearlogButton_Click);
+            // 
+            // lblLastAction
+            // 
+            this.lblLastAction.AutoSize = true;
+            this.lblLastAction.Location = new System.Drawing.Point(18, 157);
+            this.lblLastAction.Name = "lblLastAction";
+            this.lblLastAction.Size = new System.Drawing.Size(69, 13);
+            this.lblLastAction.TabIndex = 18;
+            this.lblLastAction.Text = "Last Action : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 203);
+            this.Controls.Add(this.lblLastAction);
+            this.Controls.Add(this.ClearlogButton);
+            this.Controls.Add(this.ViewlogButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ActionDelayTextBox);
+            this.Controls.Add(this.LogStringTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LogFolderTextBox);
+            this.Controls.Add(this.StartButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoClickOnLog";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox LogFolderTextBox;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox LogStringTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ActionDelayTextBox;
+        private System.Windows.Forms.Button ViewlogButton;
+        private System.Windows.Forms.Button ClearlogButton;
+        private System.Windows.Forms.Label lblLastAction;
+    }
+}
+
---
+++ ForRadiant/AutoClickOnLog/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Globalization;
+
+namespace AutoClickOnLog
+{
+    public partial class Form1 : Form
+    {
+        private string logFolder; // Update this with the path to your log folder
+        private string today;
+        private string[] logFileNames; // Array to store target log file names
+        private bool[] initialDetectionDone; // Flag to indicate whether initial detection has been done for each log file
+        private Thread[] monitoringThreads; // Array to store monitoring threads
+        private bool waitingForOccurrence = false; // Flag to indicate if waiting for occurrence
+        private DateTime lastOccurrenceTime; // Time of the last occurrence
+        private string logFilePath; // Path to log file
+
+        public Form1()
+        {
+            InitializeComponent();
+            logFolder = LogFolderTextBox.Text;
+            UpdateToday(); // Initialize today variable
+            initialDetectionDone = new bool[4];
+            logFilePath = Path.Combine(Application.StartupPath, "log.txt");
+        }
+        private void UpdateToday()
+        {
+            today = DateTime.Now.ToString("yyyyMMdd");
+        }
+
+
+        private volatile bool stopRequested = false;
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            // Update UI
+            lblStatus.Text = "Status: Monitoring";
+            StartButton.Enabled = false;
+            StopButton.Enabled = true;
+
+            LogFolderTextBox.Enabled = false;
+            LogStringTextBox.Enabled = false;
+            ActionDelayTextBox.Enabled = false;
+
+            // Set target log file names
+            logFileNames = GetLogFileNames(logFolder);
+
+            // Reset initial detection flags
+            Array.Clear(initialDetectionDone, 0, initialDetectionDone.Length);
+
+            // Start monitoring threads for each log file
+            monitoringThreads = new Thread[logFileNames.Length]; // Create a new array for threads
+            stopRequested = false; // Reset stopRequested flag
+            for (int i = 0; i < logFileNames.Length; i++)
+            {
+                int index = i; // Capture index variable for thread lambda
+                Thread monitoringThread = new Thread(() => MonitorLogFile(index));
+                monitoringThread.Start();
+                monitoringThreads[index] = monitoringThread; // Store the reference to the thread
+            }
+        }
+
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            // Update UI
+            lblStatus.Text = "Status: Stopped...";
+            StartButton.Enabled = true;
+            StopButton.Enabled = false;
+
+            LogFolderTextBox.Enabled = true;
+            LogStringTextBox.Enabled = true;
+            ActionDelayTextBox.Enabled = true;
+
+            // Request stop for monitoring threads
+            stopRequested = true;
+
+            // Wait for all monitoring threads to finish
+            foreach (var thread in monitoringThreads)
+            {
+                thread.Join(); // Wait for the thread to finish
+            }
+
+            // Reset monitoring threads array
+            monitoringThreads = new Thread[logFileNames.Length];
+        }
+
+        private string[] GetLogFileNames(string folder)
+        {
+            string[] allFiles = Directory.GetFiles(folder, $"{today}_*.log");
+            List<string> targetFiles = new List<string>();
+
+            foreach (string file in allFiles)
+            {
+                string fileName = Path.GetFileName(file);
+                if (fileName.EndsWith("Mlog_Ch1.log") ||
+                    fileName.EndsWith("Mlog_Ch2.log") ||
+                    fileName.EndsWith("Mlog_Ch3.log") ||
+                    fileName.EndsWith("Mlog_Ch4.log"))
+                {
+                    targetFiles.Add(fileName);
+                }
+            }
+
+            return targetFiles.ToArray();
+        }
+
+        private void MonitorLogFile(int index)
+        {
+            // Check if it's past midnight, update the today variable if needed
+            if (DateTime.Now.Date != DateTime.ParseExact(today, "yyyyMMdd", CultureInfo.InvariantCulture))
+            {
+                UpdateToday();
+            }
+
+
+            string logFileName = logFileNames[index];
+            string logFilePath = Path.Combine(logFolder, logFileName);
+
+            if (!File.Exists(logFilePath))
+            {
+                File.AppendAllText(logFilePath, "");
+            }
+
+            long lastReadLength = new FileInfo(logFilePath).Length - 1024;
+            if (lastReadLength < 0) lastReadLength = 0;
+
+            while (!stopRequested)
+            {
+                try
+                {
+                    long fileSize = new FileInfo(logFilePath).Length;
+
+                    if (fileSize > lastReadLength)
+                    {
+                        using (FileStream fs = new FileStream(logFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                        {
+                            fs.Seek(lastReadLength, SeekOrigin.Begin);
+                            byte[] buffer = new byte[1024];
+
+                            while (true)
+                            {
+                                int bytesRead = fs.Read(buffer, 0, buffer.Length);
+                                lastReadLength += bytesRead;
+                                if (bytesRead == 0) break;
+                                string text = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                                if (!initialDetectionDone[index])
+                                {
+                                    // Skip initial detection
+                                    initialDetectionDone[index] = true;
+                                    continue;
+                                }
+
+                                if (text.Contains(LogStringTextBox.Text) && !waitingForOccurrence)
+                                {
+                                    waitingForOccurrence = true;
+                                    lastOccurrenceTime = DateTime.Now;
+
+                                    int waitTime;
+                                    if (!int.TryParse(ActionDelayTextBox.Text, out waitTime))
+                                    {
+                                        waitTime = 1;
+                                    }
+                                    Thread.Sleep(waitTime * 1000);
+
+                                    string channelName = logFileName.EndsWith("Mlog_Ch1.log") || logFileName.EndsWith("Mlog_Ch2.log") ? "Ch1 or Ch2" : "Ch3 or Ch4";
+                                    string oppositeChannelName = logFileName.EndsWith("Mlog_Ch1.log") || logFileName.EndsWith("Mlog_Ch2.log") ? "Ch3 or Ch4" : "Ch1 or Ch2";
+                                    string processName = logFileName.EndsWith("Mlog_Ch1.log") || logFileName.EndsWith("Mlog_Ch2.log") ? "RunCh3Ch4.exe" : "RunCh1Ch2.exe";
+                                    string appPath = Path.Combine(Application.StartupPath, processName);
+
+                                    LogToFile($"Starting {processName} based on occurrence in {channelName}.");
+                                    Invoke((MethodInvoker)delegate
+                                    {
+                                        lblLastAction.Text = $"Last action : {DateTime.Now}: Finished {channelName}, Start {oppositeChannelName}.\"";
+                                    });
+
+
+                                    try
+                                    {
+                                        Process.Start(appPath);
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        LogToFile($"Error starting {processName}: {ex.Message}");
+                                    }
+
+                                    waitingForOccurrence = false;
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LogToFile($"Error monitoring log file: {ex.Message}");
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void LogToFile(string message)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(logFilePath, true))
+                {
+                    writer.WriteLine($"{DateTime.Now}: {message}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error writing to log file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ClearlogButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                File.WriteAllText(logFilePath, "");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error clearing to log file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ViewlogButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start(logFilePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error opening log file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Check if monitoringThreads is not null
+            //
+            if (monitoringThreads != null)
+            {
+                // Set stopRequested flag to true to request stop monitoring
+                stopRequested = true;
+
+                // Wait for all monitoring threads to finish, but with a timeout
+                DateTime endTime = DateTime.Now.AddSeconds(10); // Set a timeout of 10 seconds
+                foreach (var thread in monitoringThreads)
+                {
+                    if (!thread.Join((int)(endTime - DateTime.Now).TotalMilliseconds))
+                    {
+                        // If the thread does not finish within the timeout, abort it
+                        thread.Abort();
+                    }
+                }
+            }
+
+            // Dispose of other resources if needed
+        }
+
+    }
+}
