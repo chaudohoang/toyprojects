139c9d547f1e464c2e69168b1d51f8051d3956e9
Do Hoang Chau <dh.chau@radiantvs.com>
2025-02-27 20:46:33 +0700

    Modified   CompileCode/CompileCode/Program.cs Modified   CompileCode/CompileCode/bin/Debug/CompileCode.exe Modified   CompileCode/CompileCode/bin/Debug/CompileCode.pdb Modified   CompileCode/CompileCode/bin/Debug/RenameFiles.cs Modified   CompileCode/CompileCode/bin/Debug/RenameFiles_cs.exe

--- CompileCode/CompileCode/Program.cs
+++ CompileCode/CompileCode/Program.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Globalization;
 using System.CodeDom.Compiler;
-using System.Text;
 using Microsoft.CSharp;
 using Microsoft.VisualBasic;
 
@@ -15,101 +14,75 @@ namespace CodeProviders
         {
             if (args.Length > 0)
             {
-                //  First parameter is the source file name.
                 if (File.Exists(args[0]))
                 {
                     CompileExecutable(args[0]);
                 }
                 else
                 {
-                    Console.WriteLine("Input source file not found - {0}",
-                        args[0]);
+                    Console.WriteLine($"Input source file not found - {args[0]}");
                 }
             }
             else
             {
                 Console.WriteLine("Input source file not specified on command line!");
             }
+
+            // Keep console window open
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
         }
 
-        public static bool CompileExecutable(String sourceName)
+        public static bool CompileExecutable(string sourceName)
         {
             FileInfo sourceFile = new FileInfo(sourceName);
             CodeDomProvider provider = null;
             bool compileOk = false;
 
             // Select the code provider based on the input file extension.
-            if (sourceFile.Extension.ToUpper(CultureInfo.InvariantCulture) == ".CS")
+            switch (sourceFile.Extension.ToUpper(CultureInfo.InvariantCulture))
             {
-                provider = CodeDomProvider.CreateProvider("CSharp");
+                case ".CS":
+                    provider = CodeDomProvider.CreateProvider("CSharp");
+                    break;
+                case ".VB":
+                    provider = CodeDomProvider.CreateProvider("VisualBasic");
+                    break;
+                default:
+                    Console.WriteLine("Source file must have a .cs or .vb extension");
+                    return false;
             }
-            else if (sourceFile.Extension.ToUpper(CultureInfo.InvariantCulture) == ".VB")
+
+            // Format the executable file name with timestamp for uniqueness
+            string exeName = Path.Combine(Environment.CurrentDirectory,
+                $"{Path.GetFileNameWithoutExtension(sourceFile.Name)}_{DateTime.Now:yyyyMMddHHmmss}.exe");
+
+            CompilerParameters cp = new CompilerParameters
             {
-                provider = CodeDomProvider.CreateProvider("VisualBasic");
+                GenerateExecutable = true,
+                OutputAssembly = exeName,
+                GenerateInMemory = false,
+                TreatWarningsAsErrors = false
+            };
+
+            // Compile the source file
+            CompilerResults cr = provider.CompileAssemblyFromFile(cp, sourceName);
+
+            if (cr.Errors.Count > 0)
+            {
+                Console.WriteLine($"Errors building {sourceName} into {cr.PathToAssembly}:");
+                foreach (CompilerError ce in cr.Errors)
+                {
+                    Console.WriteLine($"  {ce}");
+                }
+                Console.WriteLine($"Total Errors: {cr.Errors.Count}");
             }
             else
             {
-                Console.WriteLine("Source file must have a .cs or .vb extension");
+                Console.WriteLine($"Source {sourceName} built into {cr.PathToAssembly} successfully.");
+                compileOk = true;
             }
 
-            if (provider != null)
-            {
-
-                // Format the executable file name.
-                // Build the output assembly path using the current directory
-                // and <source>_cs.exe or <source>_vb.exe.
-
-                String exeName = String.Format(@"{0}\{1}.exe",
-                    System.Environment.CurrentDirectory,
-                    sourceFile.Name.Replace(".", "_"));
-
-                CompilerParameters cp = new CompilerParameters();
-
-                // Generate an executable instead of
-                // a class library.
-                cp.GenerateExecutable = true;
-
-                // Specify the assembly file name to generate.
-                cp.OutputAssembly = exeName;
-
-                // Save the assembly as a physical file.
-                cp.GenerateInMemory = false;
-
-                // Set whether to treat all warnings as errors.
-                cp.TreatWarningsAsErrors = false;
-
-                // Invoke compilation of the source file.
-                CompilerResults cr = provider.CompileAssemblyFromFile(cp,
-                    sourceName);
-
-                if (cr.Errors.Count > 0)
-                {
-                    // Display compilation errors.
-                    Console.WriteLine("Errors building {0} into {1}",
-                        sourceName, cr.PathToAssembly);
-                    foreach (CompilerError ce in cr.Errors)
-                    {
-                        Console.WriteLine("  {0}", ce.ToString());
-                        Console.WriteLine();
-                    }
-                }
-                else
-                {
-                    // Display a successful compilation message.
-                    Console.WriteLine("Source {0} built into {1} successfully.",
-                        sourceName, cr.PathToAssembly);
-                }
-
-                // Return the results of the compilation.
-                if (cr.Errors.Count > 0)
-                {
-                    compileOk = false;
-                }
-                else
-                {
-                    compileOk = true;
-                }
-            }
             return compileOk;
         }
     }
