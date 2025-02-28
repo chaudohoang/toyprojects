e7d8057fe61e41ccfc96a6fd2d3a0bca0e8726ba
dh.chau <dh.chau@radiantvs.com>
2025-02-28 12:02:04 +0700

    Deleted    CompileCode/CompileCode/bin/Debug/Compiled/CompileCode.exe Added      ForRadiant/QuickTools/VignetingOff/App.config Added      ForRadiant/QuickTools/VignetingOff/Program.cs Added      ForRadiant/QuickTools/VignetingOff/Properties/AssemblyInfo.cs Added      ForRadiant/QuickTools/VignetingOff/VignetingOff.csproj Added      ForRadiant/QuickTools/VignetingOff/bin/Debug/VignetingOff.exe Added      ForRadiant/QuickTools/VignetingOff/bin/Debug/VignetingOff.exe.config Added      ForRadiant/QuickTools/VignetingOff/bin/Debug/VignetingOff.pdb Added      ForRadiant/QuickTools/VignetingOn/App.config Added      ForRadiant/QuickTools/VignetingOn/Program.cs Added      ForRadiant/QuickTools/VignetingOn/Properties/AssemblyInfo.cs Added      ForRadiant/QuickTools/VignetingOn/VignetingOn.csproj Added      ForRadiant/QuickTools/VignetingOn/bin/Debug/VignetingOn.exe Added      ForRadiant/QuickTools/VignetingOn/bin/Debug/VignetingOn.exe.config Added      ForRadiant/QuickTools/VignetingOn/bin/Debug/VignetingOn.pdb Modified   ForRadiant/ForRadiant.sln

---
+++ ForRadiant/QuickTools/VignetingOn/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class ModifyIniFiles
+{
+    static void Main()
+    {
+        string folderPath = Directory.GetCurrentDirectory();
+        string[] iniFiles = Directory.GetFiles(folderPath, "*.ini");
+        string newValue = "1"; // Automatically set to 1
+
+        foreach (string iniFile in iniFiles)
+        {
+            if (Regex.IsMatch(Path.GetFileName(iniFile), "DX", RegexOptions.IgnoreCase)) continue;
+
+            var lines = File.ReadAllLines(iniFile).ToList();
+            bool modified = false;
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                Match match = Regex.Match(lines[i], "^STEP_3_VIGNETTING_CORRECTION\\s*=\\s*(\\d+\\.?\\d*)");
+                if (match.Success)
+                {
+                    string oldValue = match.Groups[1].Value;
+                    lines[i] = Regex.Replace(lines[i], "(?<=STEP_3_VIGNETTING_CORRECTION\\s*=\\s*)\\d+\\.?\\d*", newValue);
+                    modified = true;
+                    Console.WriteLine($"Updated: {Path.GetFileName(iniFile)} (STEP_3_VIGNETTING_CORRECTION {oldValue} -> {newValue})");
+                }
+            }
+
+            if (modified)
+            {
+                File.WriteAllLines(iniFile, lines);
+            }
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
